8d791c9b6459eebc1af6eb78a2d54b623eb61292
SaadNabeel <saadnabeel2010@gmail.com>
2024-09-21 15:04:04 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+try:
+    a=int(input("Enter a:"))
+    if a%2==0:
+        raise ValueError
+    print("Number is",a)
+
+except ValueError:
+    print("Error is:Number is divisible by 2")
+
+else:
+    print("Successful")
+
+finally:
+    print("Program Ended")
